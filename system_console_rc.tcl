8e6f6b5c08aa052c917b461abf7f7fee2c1b9531
Yuriy VG <yuravg@gmail.com>
2021-02-18 12:00:37 +0300

    Add environment configuration file
    Add file to customize System Console environment.

---
+++ system_console_rc.tcl
@@ -0,0 +1,11 @@
+# Altera System Console's RC script
+#
+# This TCL file is installed as:
+#   $HOME/system_console/system_console_rc.tcl
+#
+# On launch, if this TCL file is found in the above location, System Console
+# will call the TCL command "source <this file>" upon entry.  This is intended
+# to be a configuration file that allows users to handily source scripts and
+# perform commands they would want to set up their System Console environment.
+#
+# Please edit this script to your preference.
